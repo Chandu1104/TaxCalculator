2bcba5fe5e8a602cb3c170547550832a35581679
Chandu1104 <35008864+Chandu1104@users.noreply.github.com>
2022-09-14 14:53:11 +0530

    First push

---
+++ script.js
@@ -0,0 +1,25 @@
+let packag;
+let hra = 0;
+let nps = 0;
+let pf = 0;
+let vpf = 0;
+let lic = 0;
+let foodReimbursement =0;
+let medicalInsurance = 0;
+let standardDeduction = 0;
+let mutualFund = 0;
+let bankSavings = 0;
+let taxbfrHraNps= 0;
+let taxaftHraNps =0;
+let other80c = 0;
+let finalDeduction =0;
+let canInvestMore = 0;
+let packDifference = 0;
+let taxSlab=0;
+
+
+function calculate(){
+    packag = document.getElementById("package").value;
+    console.log(packag)
+}
+
